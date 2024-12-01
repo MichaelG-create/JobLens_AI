ec0504aef0d561c08176f1f0745268d073085da0
micha-dv6 <michael.garcia.73@gmail.com>
2024-12-01 16:29:48 +0100

    WV EXTRACT job_list from jobs.ch to CSV (page 1 to 100) (sometimes error list index out of range during extraction : some offers don't have all required fields)

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import csv
+import re
+import time
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+
 # from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-
-import time
+from selenium.webdriver.common.by import By
 
 # Importer la fonction format_date depuis date_utils.py
 from date_utils import format_date
+
 
 # Fonction pour charger les clés uniques existantes dans le CSV
 def load_existing_keys(csv_file):
     existing_keys = set()
     try:
-        with open(csv_file, mode='r', newline='', encoding='utf-8') as file:
+        with open(csv_file, mode="r", newline="", encoding="utf-8") as file:
             reader = csv.reader(file)
             next(reader)  # Skip the header row
             for row in reader:
@@ -22,6 +25,7 @@ def load_existing_keys(csv_file):
         # Si le fichier n'existe pas, retourner un set vide
         return existing_keys
     return existing_keys
+
 
 # Configurer les options pour utiliser Chrome en mode headless
 chrome_options = Options()
@@ -32,19 +36,32 @@ driver = webdriver.Chrome(options=chrome_options)
 
 # Chargez la première page
 page_num = 1
-csv_file = 'job_offers.csv'
+csv_file = "job_offers.csv"
 existing_keys = load_existing_keys(csv_file)
 
 # Préparer le fichier CSV et charger les clés existantes
-with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
+with open(csv_file, mode="a", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
 
     # Si le fichier est vide, ajouter l'en-tête
     if file.tell() == 0:
-        writer.writerow(["Unique Key", "Titre", "Date Publiée", "Localisation", "Temps Plein", "Contrat", "Entreprise"])
+        writer.writerow(
+            [
+                "Unique Key",
+                "Titre",
+                "Date Publiée",
+                "Localisation",
+                "Temps Plein",
+                "Contrat",
+                "Entreprise",
+            ]
+        )
 
-    while page_num <= 100 :
-        page_url=f"https://www.jobs.ch/fr/offres-emplois/?page={page_num}&term=data+engineer"
+    max_page = 100
+    while page_num <= max_page:
+        page_url = (
+            f"https://www.jobs.ch/fr/offres-emplois/?page={page_num}&term=data+engineer"
+        )
         print(f"analysing page : {page_url}")
         driver.get(page_url)
         time.sleep(3)  # Ajouter un délai pour permettre au contenu de se charger
@@ -60,17 +77,29 @@ with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
         # Extraire les détails et les enregistrer dans le fichier CSV
         for offer in offers:
             try:
-                offer_link = offer.find_element(By.CSS_SELECTOR, "a[data-cy='job-link']")
+                offer_link = offer.find_element(
+                    By.CSS_SELECTOR, "a[data-cy='job-link']"
+                )
                 # Extraire l'attribut 'href'
-                job_link = offer_link.get_attribute('href')
+                job_link = offer_link.get_attribute("href")
                 # job_link = offer.find_elements(By.XPATH, "//*[@href='searched_jobs']")
 
                 title = offer.find_element(By.CLASS_NAME, "mb_s8").text  # Titre
-                date_published = format_date(offer.find_elements(By.CLASS_NAME, "mb_s12")[0].text)  # Date publiée
-                location = offer.find_elements(By.CLASS_NAME, "mb_s12")[1].text  # Localisation
-                full_time = offer.find_elements(By.CLASS_NAME, "mb_s12")[2].text  # Temps plein
-                contract = offer.find_elements(By.CLASS_NAME, "mb_s12")[3].text  # Contrat
-                company = offer.find_elements(By.CLASS_NAME, "mb_s12")[4].text  # Entreprise
+                date_published = format_date(
+                    offer.find_elements(By.CLASS_NAME, "mb_s12")[0].text
+                )  # Date publiée
+                location = offer.find_elements(By.CLASS_NAME, "mb_s12")[
+                    1
+                ].text  # Localisation
+                full_time = offer.find_elements(By.CLASS_NAME, "mb_s12")[
+                    2
+                ].text  # Temps plein
+                contract = offer.find_elements(By.CLASS_NAME, "mb_s12")[
+                    3
+                ].text  # Contrat
+                company = offer.find_elements(By.CLASS_NAME, "mb_s12")[
+                    4
+                ].text  # Entreprise
 
                 # Créer une clé unique en combinant les éléments
                 unique_key = f"{title} | {company} | {date_published} | {location}"
@@ -78,13 +107,50 @@ with open(csv_file, mode='a', newline='', encoding='utf-8') as file:
                 # Si la clé existe déjà, ignorer cette offre
                 if unique_key not in existing_keys:
                     # Écrire les données dans le fichier CSV
-                    writer.writerow([unique_key, title, date_published, location, full_time, contract, company, job_link])
-                    existing_keys.add(unique_key)  # Ajouter la clé unique à l'ensemble pour éviter les doublons
+                    writer.writerow(
+                        [
+                            unique_key,
+                            title,
+                            date_published,
+                            location,
+                            full_time,
+                            contract,
+                            company,
+                            job_link,
+                        ]
+                    )
+                    existing_keys.add(
+                        unique_key
+                    )  # Ajouter la clé unique à l'ensemble pour éviter les doublons
                 else:
-                    print(f"Doublon trouvé, l'offre '{title}' de '{company}' est déjà présente.")
+                    print(
+                        f"Doublon trouvé, l'offre '{title}' de '{company}' est déjà présente."
+                    )
 
             except Exception as e:
                 print(f"Erreur lors de l'extraction : {e}")
+
+        # # Vérifier si la page suivante existe en utilisant le bouton "Next"
+        if page_num == 1:
+            try:
+                page_indicator_element = driver.find_element(
+                    By.CSS_SELECTOR, "span[title^='Page']"
+                )  # Sélecteur qui match avec les spans "Page X"
+                page_text = (
+                    page_indicator_element.text
+                )  # Extraire le texte de l'élément, comme " ... 84"
+
+                # Utiliser une expression régulière pour extraire le numéro de page à partir du texte
+                match = re.search(r"\.\.\. (\d+)", page_text)  # Chercher "... XXX"
+                if match:
+                    last_page_number = match.group(1)  # Le numéro de la dernière page
+                    print(f"Le numéro de la dernière page est : {last_page_number}")
+                    max_page = int(last_page_number)
+                else:
+                    print("Le numéro de la dernière page n'a pas pu être extrait.")
+
+            except Exception as e:
+                print(f"Erreur : {e}")
 
         # # Vérifier si la page suivante existe en utilisant le bouton "Next"
         # try:
